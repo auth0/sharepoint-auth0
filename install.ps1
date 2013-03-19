6a8fa05cf99483595b07e89ee4d3bd5b2799d609
Matias Woloski <matiasw@gmail.com>
2013-03-18 23:11:02 -0300

    import the module on installation

--- install.ps1
+++ install.ps1
@@ -2,10 +2,11 @@ $modulespath = ($env:psmodulepath -split ";")[0]
 $auth0path = "$modulespath\Auth0"
 
 Write-Host "Creating module directory"
-New-Item -Type Container -Force -path $pswatchpath | out-null
+New-Item -Type Container -Force -path $auth0path | out-null
 
 Write-Host "Downloading and installing"
-(new-object net.webclient).DownloadString("https://raw.github.com/auth0/sharepoint-auth0/master/Auth0.psm1") | Out-File "$auth0\Auth0.psm1" 
+(new-object net.webclient).DownloadString("https://raw.github.com/auth0/sharepoint-auth0/master/Auth0.psm1") | Out-File "$auth0path\Auth0.psm1" 
 
-Write-Host "Auth0 PowerShell Module Installed!"
-Write-Host 'Use "Import-Module Auth0" and then Enable-Auth0'
+Write-Host "Auth0 PowerShell Module Installed and imported!"
+
+Import-Module "$auth0path\Auth0.psm1"
