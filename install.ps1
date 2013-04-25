d08c405a01f2bd7b6a6808c6d61db9e37b3a77bc
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-04-25 07:56:12 -0700

    download wsp file

--- install.ps1
+++ install.ps1
@@ -5,7 +5,9 @@ Write-Host "Creating module directory"
 New-Item -Type Container -Force -path $auth0path | out-null
 
 Write-Host "Downloading and installing"
-(new-object net.webclient).DownloadString("https://raw.github.com/auth0/sharepoint-auth0/master/Auth0.psm1") | Out-File "$auth0path\Auth0.psm1" 
+$webclient = new-object net.webclient
+$webclient.DownloadString("https://raw.github.com/auth0/sharepoint-auth0/master/Auth0.psm1") | Out-File "$auth0path\Auth0.psm1"
+$webclient.DownloadString("https://raw.github.com/auth0/sharepoint-claimsprovider/master/Auth0.ClaimsProvider/bin/Release/Auth0.ClaimsProvider.wsp") | Out-File "$auth0path\Auth0.ClaimsProvider.wsp"
 
 if (Get-Module "Auth0") { Remove-Module "Auth0" }
 
