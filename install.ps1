9c477d64c9cc0eb0c92780973f34eb74341f59c2
Sebastian Iacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-04-25 20:02:20 -0300

    minor improvements

--- install.ps1
+++ install.ps1
@@ -9,7 +9,9 @@ $webclient = new-object net.webclient
 $webclient.DownloadString("https://raw.github.com/auth0/sharepoint-auth0/master/Auth0.psm1") | Out-File "$auth0path\Auth0.psm1"
 $webclient.DownloadFile("https://raw.github.com/auth0/sharepoint-claimsprovider/master/Auth0.ClaimsProvider/bin/Release/Auth0.ClaimsProvider.wsp", "$auth0path\Auth0.ClaimsProvider.wsp")
 
-if (Get-Module "Auth0") { Remove-Module "Auth0" }
+if (Get-Module "Auth0") { 
+	Remove-Module "Auth0" 
+}
 
 Import-Module "$auth0path\Auth0.psm1"
 
